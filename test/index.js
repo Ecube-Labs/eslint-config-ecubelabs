36fff186b1eb6ed776722d92de205be73b96ef0f
Charlie <id.kim@ecubelabs.com>
2018-10-29 19:09:16 +0900

    Change rules

---
+++ test/index.js
@@ -0,0 +1,5 @@
+// this is very very long commeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeents
+
+const n = 1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789;
+
+exports.n = n;
