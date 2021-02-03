c13f5771c9676b29c3a122e669155227dccc87d6
Tim <sh.kang@ecubelabs.com>
2021-02-03 16:08:24 +0900

    fix: 테스트 코드 수정

--- test/ts-no-unused-vars.ts
+++ test/ts-no-unused-vars.ts
@@ -1,2 +1,5 @@
+const testFn = (_test: (...args: any[]) => void) => {}; // 타입인 args가 걸리지 않으면 된다.
+
 // FIXME: https://stackoverflow.com/questions/55807329/why-eslint-throws-no-unused-vars-for-typescript-interface 왜 난 해결이 안되지?
 type AnyType = Record<string, unknown>;
+interface AnyInterface {}
